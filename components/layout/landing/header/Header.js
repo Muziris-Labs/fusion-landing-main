408f3b30c3112b53fa038ea1739e89022996d9c6
Gautam Raj <ashragautam25@gmail.com>
2024-08-18 10:34:44 +0530

    refactor: Update landing page structure and styles
    This commit refactors the landing page by updating the structure and styles. It adds a new header component (Header.js) and adjusts the layout to improve responsiveness. Additionally, it adds a max width to sections and replaces partner's texts with logos. These changes enhance the overall user experience and visual appeal of the landing page.

--- components/layout/landing/header/Header.js
+++ components/layout/landing/header/Header.js
@@ -8,50 +8,53 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="m-4 sm:m-8 p-8 md:p-10 lg:p-12 xl:p-16 3xl:p-20 space-y-32 lg:space-y-48 3xl:space-y-64 bg-dark-tertiary rounded-3xl header 2xl:m-auto 2xl:my-8 max-w-screen-2xl">
-      <Logo />
+    <header className="p-4 sm:p-8 2xl:h-screen max-w-screen-2xl 2xl:m-auto 2xl:py-8">
+      <div className="h-full p-8 md:p-10 lg:p-12 xl:p-16 3xl:p-20 space-y-32 lg:space-y-48 3xl:space-y-64 bg-dark-tertiary rounded-3xl header 2xl:flex 2xl:flex-col 2xl:justify-between">
+        <Logo />
 
-      <section className="md:space-y-8">
-        <div className="block w-full md:hidden">
-          <Image
-            src="/landing/header/fusion-hero.png"
-            width={600}
-            height={600}
-            alt="Hero Image"
-            className="-translate-y-20 mx-auto"
-          />
-        </div>
+        <section className="md:space-y-8">
+          <div className="block w-full md:hidden">
+            <Image
+              src="/landing/header/fusion-hero.png"
+              width={600}
+              height={600}
+              alt="Hero Image"
+              className="-translate-y-20 mx-auto"
+            />
+          </div>
 
-        <div className="space-y-2 -translate-y-8">
-          <h1 className="text-5xl sm:text-6xl md:text-5xl lg:text-6xl xl:text-7xl 3xl:text-8xl  font-bold">
-            ZK-Powered <br /> Fortress
-          </h1>
+          <div className="space-y-2 -translate-y-8">
+            <h1 className="text-5xl sm:text-6xl md:text-5xl lg:text-6xl xl:text-7xl 3xl:text-8xl  font-bold">
+              ZK-Powered <br /> Fortress
+            </h1>
 
-          <p className="paragraph max-w-sm lg:max-w-lg 2xl:max-w-xl">
-            Deploy your zk-powered multi-chain smart wallet powered by Wormhole
-          </p>
-        </div>
+            <p className="paragraph max-w-sm lg:max-w-lg 2xl:max-w-xl">
+              Deploy your zk-powered multi-chain smart wallet powered by
+              Wormhole
+            </p>
+          </div>
 
-        <div className="flex space-x-4">
-          <RippleLink
-            location="/#"
-            color="#000"
-            className="bg-[#D9D9D9] font-semibold capitalize"
-          >
-            Launch App
-          </RippleLink>
+          <div className="flex space-x-4">
+            <RippleLink
+              location="/#"
+              color="#000"
+              className="bg-[#D9D9D9] font-semibold capitalize"
+            >
+              Launch App
+            </RippleLink>
 
-          <RippleLink
-            location="/#"
-            border
-            color="#D9D9D9"
-            rippleColor="rgba(217,217,217,0.2)"
-            className="font-semibold capitalize"
-          >
-            Join Wishlist
-          </RippleLink>
-        </div>
-      </section>
+            <RippleLink
+              location="/#"
+              border
+              color="#D9D9D9"
+              rippleColor="rgba(217,217,217,0.2)"
+              className="font-semibold capitalize"
+            >
+              Join Wishlist
+            </RippleLink>
+          </div>
+        </section>
+      </div>
     </header>
   );
 };
